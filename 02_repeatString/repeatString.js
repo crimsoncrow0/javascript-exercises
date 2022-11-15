815d7bb96ad9b637dd53275963bdc35b38a059ac
craig-gardner <craig.r.gardner@proton.me>
2022-11-15 22:13:24 +0100

    Function passes test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
 
+const repeatString = function(x, num) {
+    let repeatedString = '';
+    for (let i = 1; i <= num; i++) {
+        repeatedString += x;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
