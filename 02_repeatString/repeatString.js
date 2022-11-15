2a4a4068b472cae59b532042646ecb2c50f96bc0
craig-gardner <craig.r.gardner@proton.me>
2022-11-15 22:22:05 +0100

    Modify function to return 'ERROR' for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,17 @@
 
 const repeatString = function(x, num) {
     let repeatedString = '';
-    for (let i = 1; i <= num; i++) {
-        repeatedString += x;
+
+    if (num >= 0) {
+        for (let i = 1; i <= num; i++) {
+            repeatedString += x;
+        }
+        return repeatedString;
     }
-    return repeatedString;
+    else {
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
