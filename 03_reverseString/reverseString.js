34d72a7c8ed8fc7a0360059feb30061d2363d2cc
craig-gardner <craig.r.gardner@proton.me>
2022-11-16 20:47:42 +0100

    Create reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(x) {
+    reversed = '';
+    stringLength = x.length;
+    for (let i = stringLength; i > 0; i--) {
+        reversed += x[i-1];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
