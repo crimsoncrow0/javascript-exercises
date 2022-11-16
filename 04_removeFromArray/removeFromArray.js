488b69898ff0d0138738290ef9138d3b4b75bba8
craig-gardner <craig.r.gardner@proton.me>
2022-11-16 21:55:19 +0100

    Create basic function to pass initial test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a,x) {
+    let ind = a.indexOf(x);
+    a.splice(ind,1);
+    return a;
 };
 
 // Do not edit below this line
