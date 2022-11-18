04f6b9c1e9a4e5c7658a0443f1920b027cac11f2
craig-gardner <craig.r.gardner@proton.me>
2022-11-18 18:46:37 +0100

    Update sumAll function to return error for non-numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 const sumAll = function(x,y) {
     let total = 0;
 
-    if (x >= 0 && y >= 0) {
+    if (x >= 0 && y >= 0 && typeof x === "number" && typeof y === "number") {
         if (x < y) {
             for (let i = x; i <= y; i++) {
                 total += i;
