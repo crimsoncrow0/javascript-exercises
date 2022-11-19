8f9b4ac81a25dd02cbde42c58d8293c9de9b23db
craig-gardner <craig.r.gardner@proton.me>
2022-11-19 19:32:40 +0100

    Condense functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,11 @@
 const ftoc = function(f) {
   let c = ((f - 32) * 5 / 9);
-  c = Math.round(c * 10) / 10;
-  return c;
+  return Math.round(c * 10) / 10;
 };
 
 const ctof = function(c) {
   let f = (c * 9 / 5 + 32);
-  f = Math.round(f * 10) / 10
-  return f;
+  return Math.round(f * 10) / 10
 };
 
 // Do not edit below this line
