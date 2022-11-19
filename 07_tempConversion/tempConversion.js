b35e5c37ba5c8312cd583e17ee927cbaeb5b3a61
craig-gardner <craig.r.gardner@proton.me>
2022-11-19 19:21:27 +0100

    Update functions to round to 1 decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
 const ftoc = function(f) {
   let c = ((f - 32) * 5 / 9);
+  c = Math.round(c * 10) / 10;
   return c;
 };
 
 const ctof = function(c) {
   let f = (c * 9 / 5 + 32);
+  f = Math.round(f * 10) / 10
   return f;
 };
 
