d4e2c9e12c0e592cf82f4df52b723b553a892e02
craig-gardner <craig.r.gardner@proton.me>
2023-01-05 19:02:46 +0100

    Create initial function. First test passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+function reverse(stringEnd) {
+    let reversed = '';
+    for (let i = stringEnd.length - 1; i >= 0; i--) {
+        reversed += stringEnd[i];
+    };
+    return reversed;
+}
+
+const palindromes = function (palindrome) {
+    //lowercase
+    let text = palindrome.toLowerCase()
+    // remove characters and spaces
+    text = text.replace('/ /g','');
+    text = text.replace('/!/g','');
+    text = text.replace('/,/g','');
+    let length = Math.round(text.length);
+    let start = text.slice(0, Math.round(text.length / 2));
+    let end = text.slice(Math.floor(text.length / 2) );
+
+    // reverse second half of string
+    const endReversed = reverse(end);
+
+     //if first half = reverse of second half, output true
+    if (endReversed === start) {
+        return true
+    }
+    return false
 
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
