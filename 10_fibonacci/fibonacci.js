61bdfef784594faf7cabbaafaec185c16d6f5183
craig-gardner <craig.r.gardner@proton.me>
2023-01-05 18:10:08 +0100

    Create function. All tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+    if (count < 0) return "OOPS";
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < count; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b
 };
 
 // Do not edit below this line
