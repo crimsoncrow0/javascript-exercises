3cac6c6fc81551ebdd33c6a1c95540830bade0c8
craig-gardner <craig.r.gardner@proton.me>
2023-01-22 14:09:54 +0100

    Change function to arrow function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,9 +1,10 @@
 const findTheOldest = function(array) {
-    const oldest = array.sort(function(a,b){
+    
+    const oldest = array.sort((a,b) => {
         const first = a.yearOfDeath - a.yearOfBirth;
         const next = b.yearOfDeath - b.yearOfBirth;
         return (first > next ? -1 : 1 );
-    });
+});
     return oldest[0];
 };
 
